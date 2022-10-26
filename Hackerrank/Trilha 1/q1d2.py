352ad0c87613f6fb7bafe594a4d573f5a5622ec2
Tiago Campêlo <tscs2@cin.ufpe.br>
2022-10-25 21:57:45 -0300

    Adiciona dia 2 da trilha 1

---
+++ Hackerrank/Trilha 1/q1d2.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    d = dict()
+    for n in a:
+        d[n] = d.get(n, 0) + 1
+    
+    for n in d.keys():
+        if d[n] == 1:
+            return n
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ Hackerrank/Trilha 1/q2d2.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    size = len(arr)
+    c = 0
+    r = l = 0
+    while c < size:
+        r = r + arr[c][c]
+        l = l + arr[c][size - c - 1]
+        c = c + 1
+    
+    return abs(r - l)
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
